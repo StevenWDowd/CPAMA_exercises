02cd76c2810bf4992814884f1a94a6bec632e64e
Steven Dowd <swdowd@gmail.com>
2024-12-30 18:02:26 -0500

    basic exercises

---
+++ ch2/var_test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    int first;
+    int second;
+    float third;
+    float fourth;
+
+    printf("%i\n", first);
+    printf("%i\n", second);
+    printf("%f\n", third);
+    printf("%f\n", fourth);
+
+    return 0;
+}
