b3f47a47b2456188bb56f1be5a78eeb183fe5f53
Steven Dowd <swdowd@gmail.com>
2025-01-07 21:15:17 -0500

    practice

--- ch3/prod_info.c
+++ ch3/prod_info.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+void prodInfo();
+
 int main(void){
+    prodInfo();
     return 0;
 }
 
@@ -13,5 +16,5 @@ void prodInfo(void){
     scanf("%f", &price);
     printf("Enter date (mm/dd/yyyy): ");
     scanf("%d/%d/%d", &month, &day, &year);
-
+    printf("Item\t\tUnit\t\tPurchase\n\t\tPrice\t\tDate\n%d\t\t%.2f\t\t%d/%d/%d\n", item, price, month, day, year);
 }
